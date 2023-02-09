c31545b36be6bcb8d1d98c0569f46cf31b0942f8
Barış SAVAŞ <barissavas17@gmail.com>
2023-02-09 17:55:35 +0300

    home page added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
 
 export default function Home() {  
   return (
-    <h4>Home</h4>
+    <div className="pre mt-5">
+      <p>Welcome to the Shopping Cart</p>
+      <p>In <code><i className="i-default">Store</i></code> tab you can do the shopping.</p>
+      <p>In Right Side menu you can increase, decrease and remove the shopping item.</p>
+      <p>Go to payment button redirects you to the stripe paying page.</p>
+      <p>If the payment is successful, you will be redirected to the <code><i className="i-success">Success</i></code> page.</p>
+      <p>In <code><i className="i-success">Success</i></code> Page , you can see the last payment details.</p>
+    </div>
   )
 }
