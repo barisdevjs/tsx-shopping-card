b76bddef56c69f3bae547fc6e063056eb4ceea79
Barış SAVAŞ <barissavas17@gmail.com>
2023-02-10 19:55:37 +0300

    cancel page added

--- server/server.js
+++ server/server.js
@@ -11,46 +11,44 @@ app.use(express.json());
 
 app.post('/payment', async (req, res) => {
 
-    // console.log(req.body)
-    const items = req.body.items;
-    let lineItems = [];
+  // console.log(req.body)
+  const items = req.body.items;
+  let lineItems = [];
 
-    items.forEach(item => {
-        lineItems.push({
-            price: item.id,
-            quantity: item.quantity
-        })
+  items.forEach(item => {
+    lineItems.push({
+      price: item.id,
+      quantity: item.quantity,
     })
+  })
 
-    const session = await stripe.checkout.sessions.create({
-        line_items: lineItems,
-        mode: 'payment',
-        success_url: "http://localhost:5173/success",
-        cancel_url: "http://localhost:5173/cancel"
-    });
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: "http://localhost:5173/success",
+    cancel_url: "http://localhost:5173/cancel"
+  });
 
-    res.send(JSON.stringify({
-        url: session.url
-    }));
+  res.send(JSON.stringify({
+    url: session.url
+  }));
 
 })
 
 
 app.get('/paymentIntents', async (req, res) => {
-    try {
-      const paymentIntents = await stripe.paymentIntents.list({
-        limit: 1
-      });
-  
-      res.json(paymentIntents);
-      console.log('server', paymentIntents)
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ error: error.message });
-    }
-  });
+  try {
+    const paymentIntents = await stripe.paymentIntents.list({
+      limit: 1
+    });
 
-
+    res.json(paymentIntents);
+    console.log('server', paymentIntents)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+});
 
 
 app.listen(4000, () => console.log("Listening on port 4000!"));
--- src/pages/Cancel.tsx
+++ src/pages/Cancel.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BsFillExclamationTriangleFill } from "react-icons/bs"
+
 
 function Cancel() {
   return (
-    <div>Cancel</div>
+    <div className='cancel'>
+      <BsFillExclamationTriangleFill size={200} />
+      <h1>Something went wrong</h1>
+      <h2>The page didn't load properly.Please try again .<br /><br />
+      If it doesn't load up, contact with your administrator.</h2>
+    </div>
   )
 }
 
