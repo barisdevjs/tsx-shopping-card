6d3f48f43fbbebadeac054c67607e1292b74bae6
Barış SAVAŞ <barissavas17@gmail.com>
2023-02-10 15:07:50 +0300

    dotenv && opacity animation added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 var cors = require('cors');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || '');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(cors());
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,12 @@
-
 export default function Home() {  
   return (
-    <div className="pre mt-5">
-      <p>Welcome to the Shopping Cart</p>
-      <p>In <code><i className="i-default">Store</i></code> tab you can do the shopping.</p>
-      <p>In Right Side menu you can increase, decrease and remove the shopping item.</p>
-      <p>Go to payment button redirects you to the stripe paying page.</p>
-      <p>If the payment is successful, you will be redirected to the <code><i className="i-success">Success</i></code> page.</p>
-      <p>In <code><i className="i-success">Success</i></code> Page , you can see the last payment details.</p>
+    <div className="pre mt-5 ">
+      <p className="typewriter">Welcome to the Shopping Cart</p>
+      <p className="typewriter">In <code><i className="i-default">Store</i></code> tab you can do the shopping.</p>
+      <p className="typewriter">In Right Side menu you can increase, decrease and remove the shopping item.</p>
+      <p className="typewriter">Go to payment button redirects you to the stripe paying page.</p>
+      <p className="typewriter">If the payment is successful, you will be redirected to the <code><i className="i-success">Success</i></code> page.</p>
+      <p className="typewriter">In <code><i className="i-success">Success</i></code> Page , you can see the last payment details.</p>
     </div>
   )
 }
